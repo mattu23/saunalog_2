d47ed883673e547034e5d6768267ee75dfc04232
mattu23 <hirokiii_27@yahoo.co.jp>
2023-08-19 01:23:00 +0900

    データ未入力時の処理＆リスト表示

--- front_saunalog/src/main.js
+++ front_saunalog/src/main.js
@@ -21,6 +21,7 @@ const app = Vue.createApp({
   methods: {
     addList: function(event) {
       console.log('Clicked!')
+      if(this.newItem === '') return
       let saunalog = {
         item: this.newItem
       }
