3f691c4a3c1a332c255e4166d13ff76de7444347
mattu23 <hirokiii_27@yahoo.co.jp>
2023-08-23 01:23:43 +0900

    Vue/cliの再インストール・フォームの表示処理のみの実装

--- front_saunalog/src/main.js
+++ front_saunalog/src/main.js
@@ -1,35 +1,26 @@
+// import App from './App.vue'
 import { createApp } from 'vue';
 
-// const app = createApp({
-//     data() {
-      
-//     },
-//     methods: {
-//     addList: function(event) {
-       
-//     }
-//   }
-// })
 
-// app.mount('#app')
-
-const app = Vue.createApp({
+const app = createApp({
   data: () => ({
     newItem: '',
     saunalogs: []
   }),
   methods: {
-    addList: function(event) {
-      console.log('Clicked!')
+    //データを表示するメソッド
+    addList: function() {
       if(this.newItem === '') return
       let saunalog = {
         item: this.newItem
       }
       this.saunalogs.push(saunalog)
       this.newItem = ''
+    },
+    //データを削除するメソッド
+    deleteList: function(index) {
+      this.saunalogs.splice(index, 1)
     }
   }
 })
 app.mount('#app')
-
-
