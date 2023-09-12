0ba15d02514e702a5204a4f90c524c7a356baa34
mattu23 <hirokiii_27@yahoo.co.jp>
2023-09-13 02:14:15 +0900

    authモジュールの作成・必要なnpmモジュールのインストール

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,10 +6,12 @@ const ormConfig = require('../ormconfig');
 import { SaunalogModule } from './saunalog/saunalog.module';
 // import { SaunalogController } from './saunalog/saunalog.controller';
 // import { SaunalogService } from './saunalog/saunalog.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   //usersModuleを一旦削除しています
-  imports: [TypeOrmModule.forRoot(ormConfig), SaunalogModule],
+  imports: [TypeOrmModule.forRoot(ormConfig), SaunalogModule, AuthModule, UsersModule],
   controllers: [],
   providers: [],
 })
