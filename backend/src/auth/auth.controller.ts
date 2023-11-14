acc3733d4f112b89f1139acb42ab6dbcf3451ea6
mattu23 <hirokiii_27@yahoo.co.jp>
2023-11-15 00:34:13 +0900

    コメントの追加

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -9,22 +9,26 @@ import { CredentialsDTO } from './DTO/credentials.dto';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  //ユーザー新規登録
   @Post('signup')
   async signup(@Body() CreateUserDto: CreateUserDto): Promise<User> {
     return await this.authService.signUp(CreateUserDto);
   }
 
+  //ユーザーのログイン
   @Post('login')
   async signin(@Body() credentialsDto: CredentialsDTO, @Req() req: Request) {
-    return this.authService.signIn(credentialsDto, req)
+    return await this.authService.signIn(credentialsDto, req)
   }
 
+  //ログイン時しているユーザーの情報取得
   @Get('user')
   async getUser(@Req() req:Request): Promise<User> {
     const user = await this.authService.getUserById(req.session.userId);
     return user;
   }
 
+  //ユーザーのログアウト
   @Post('logout')
   async logout(@Req() req: Request) {
     return this.authService.signOut(req);
