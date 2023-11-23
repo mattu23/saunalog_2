4423506ce63025453cc9008ca000ca41b4ccf6e8
mattu23 <hirokiii_27@yahoo.co.jp>
2023-11-23 17:08:09 +0900

    登録データの表示をリスト型に変更

--- frontend/pages/list/index.vue
+++ frontend/pages/list/index.vue
@@ -2,57 +2,29 @@
   <div id="app">
     <v-layout justify-center>
       <v-card width="600px">
-        <h1>登録したサウナログ一覧</h1>
+        <h1> サウナリスト一覧</h1>
         <h2 v-if="user">ようこそ、{{ user.username }} さん</h2>
         <v-btn to="/register"  class="primary my-5" min-width="200" style="margin: 10px;">サウナログの新規登録</v-btn>
-        <v-btn to="/editUser"  class="primary my-5" min-width="100" style="margin: 10px;">ユーザーの編集</v-btn>
         <v-container>
-          <v-row dense>
-            <v-col>
-              <v-card v-for="log in saunaLogs" :key="log.id" class="mt-5" elevation="10">
-                <v-card-title class="text-h5">
-                 施設名： {{ log.name }}
-                </v-card-title>
-                <v-card-title class="text-h5">
-                 エリア： {{ log.area }}
-                </v-card-title>
-                <v-card-title class="text-h5">
-                 評価 : {{ log.rank }}
-                </v-card-title>
-                <v-card-title class="text-h5">
-                 コメント：
-                 <br>
-                 {{ log.comment }}
-                </v-card-title>
-                  <v-btn
-                    dark
-                    :to="`/list/${log.id}`"
-                    color="green darken-1"
-                    style="margin: 10px"
-                    nuxt
-                  >
-                    編集
-                  </v-btn>
-                  <v-btn
-                    dark
-                    color="green darken-1"
-                    style="margin-left: 10px"
-                    @click="deleteLog(log.id)"
-                  >
-                    削除
-                  </v-btn>
-              </v-card>
-            </v-col>
-          </v-row>
+          <v-list dense>
+            <v-list-item-group>
+              <v-list-item v-for="log in saunaLogs" :key="log.id">
+                <v-list-item-content>
+                  <v-list-item-title style="font-size: 1.0em;">施設名：{{ log.name }}</v-list-item-title>
+                  <v-list-item-subtitle>エリア：{{ log.area }}</v-list-item-subtitle>
+                  <v-list-item-subtitle>評価：{{ log.rank }}</v-list-item-subtitle>
+                  <v-list-item-subtitle>コメント：{{ log.comment }}</v-list-item-subtitle>
+                </v-list-item-content>
+                <v-list-item-action>
+                  <v-btn :to="`/list/${log.id}`" color="green darken-1" text>編集</v-btn>
+                  <v-btn color="red darken-1" text @click="deleteLog(log.id)">削除</v-btn>
+                </v-list-item-action>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
         </v-container>
-        <v-btn
-         style="margin: 20px"
-         dark
-         color="red darken-1"
-         @click="logout()"
-         >
-         ログアウト
-        </v-btn>
+        <v-btn to="/editUser" dark color="green" min-width="100" style="margin: 10px;">ユーザーの編集</v-btn>
+        <v-btn dark color="red darken-1" @click="logout()" style="margin: 10px" >ログアウト</v-btn>
       </v-card>
     </v-layout>
   </div>
